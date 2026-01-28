4621bf64c5be2d92c9023c4143a43f6e0ea985cb
mastamond <mastamond@gmail.com>
2026-01-28 17:31:31 +0200

    inputfield

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// kanye function
 const kanyeBtn = document.getElementById("kanyeButton");
 const kanyeDiv = document.getElementById ("kanyeQuote");
 
@@ -11,4 +11,19 @@ console.log(quote);
 kanyeDiv.innerHTML = quote;
 }
 
+// strip function
+const stripBtn = document.getElementById("stripButton");
+const stripDiv = document.getElementById ("stripQuote");
+
+const strip = async function () {
+const response = await fetch("https://api.nationalize.io/?name=bock");
+const data = await response.json();
+const quote = data.quote;
+console.log(quote);
+// console.log("test");
+stripDiv.innerHTML = quote;
+}
+
+
 kanyeBtn.addEventListener("click", kanye);
+stripBtn.addEventListener("click", strip);
